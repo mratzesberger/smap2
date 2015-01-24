9b7543ec516fc49b5005edf8fa32c388fc2400a1
Mathias Ratzesberger <mathiasratzesberger@Mathiass-MBP.Speedport_W723_V_Typ_A_1_01_009>
2015-01-24 22:21:28 +0100

    Initial Commit

---
+++ smap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smap
+//
+//  Created by Mathias Ratzesberger on 24.01.15.
+//  Copyright (c) 2015 Mara-Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
